3ccc85d42ae934fe92b546d06b099d17dd9d723e
triptibhatnagar <bhatnagartripti2727@gmail.com>
2025-10-04 13:12:24 +0530

    prac6

--- Prac6/proj6/app6/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'app6'
-
-urlpatterns = [
-    path('', views.post_list, name="post_list"),
-    path('post/<int:pk>/', views.post_detail, name="post_detail"),
-]
--- Prac6/proj6/proj6/urls.py
+++ Prac6/proj6/proj6/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from app6 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app6/', include('app6.urls', namespace='app6'))
+    path('app6/', views.post_list, name="post_list"), # connect view directly
 ]
